9caee42bc7906bffffc9be7673fca08cbebdc039
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-01-15 22:36:02 +0000

    add postlike class to views.py with styling

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # because using class-based views, must use as_view() after PostList so it will render the class as a view
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 
 ]
