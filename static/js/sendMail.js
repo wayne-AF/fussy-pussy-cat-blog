ac5378a8d33b6c7e74408d5c9c42b91305c4c7df
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-01-20 23:34:28 +0000

    add background image

--- static/js/sendMail.js
+++ static/js/sendMail.js
@@ -1,3 +1,5 @@
+
+const messageContainer = document.getElementById('message')
 function sendMail(contactForm) {
     emailjs.send("service_xhz66xf", "fussypussy_template", {
         "from_name": contactForm.fullname.value,
@@ -6,9 +8,13 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
+            messageContainer.classList.add('success')
+            messageContainer.textContent = 'You submitted the form'
             return("SUCCESS", response);
         },
         function(error) {
+            messageContainer.classList.add('error')
+            messageContainer.textContent = 'You submitted the form'
             return("FAILED", error);
         }
     );
